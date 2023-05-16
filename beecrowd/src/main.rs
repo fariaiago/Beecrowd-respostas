fdd8330936d691482b2667cb61f479c68c6db9bd
fariaiago <111879008+fariaiago@users.noreply.github.com>
2023-05-16 17:20:34 +0000

    a

--- beecrowd/src/main.rs
+++ beecrowd/src/main.rs
@@ -2,34 +2,19 @@ use std::io;
 
 fn main() {
 	let mut a = String::new();
+	let mut b = String::new();
+	let mut c = String::new();
+	let mut d = String::new();
 
 	io::stdin().read_line(&mut a).unwrap();
+	io::stdin().read_line(&mut b).unwrap();
+	io::stdin().read_line(&mut c).unwrap();
+	io::stdin().read_line(&mut d).unwrap();
 
-	let a = a.trim().parse::<f64>().unwrap();
-	let mut int = (a * 100.0) as i32;
+	let a = a.trim().parse::<i32>().unwrap();
+	let b = b.trim().parse::<i32>().unwrap();
+	let c = c.trim().parse::<i32>().unwrap();
+	let d = d.trim().parse::<i32>().unwrap();
 
-	println!("NOTAS:\n{} nota(s) de R$ 100.00", int / 10000);
-	int %= 10000;
-	println!("{} nota(s) de R$ 50.00", int / 5000);
-	int %= 5000;
-	println!("{} nota(s) de R$ 20.00", int / 2000);
-	int %= 2000;
-	println!("{} nota(s) de R$ 10.00", int / 1000);
-	int %= 1000;
-	println!("{} nota(s) de R$ 5.00", int / 500);
-	int %= 500;
-	println!("{} nota(s) de R$ 2.00", int / 200);
-	int %= 200;
-
-	println!("MOEDAS:\n{} moeda(s) de R$ 1.00", int / 100);
-	int %= 100;
-	println!("{} moeda(s) de R$ 0.50", int / 50);
-	int %= 50;
-	println!("{} moeda(s) de R$ 0.25", int / 25);
-	int %= 25;
-	println!("{} moeda(s) de R$ 0.10", int / 10);
-	int %= 10;
-	println!("{} moeda(s) de R$ 0.05", int / 5);
-	int %= 5;
-	println!("{} moeda(s) de R$ 0.01", int);
+	println!("DIFERENCA = {}", a * b - c * d);
 }
