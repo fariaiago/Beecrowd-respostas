67bbb89efe5165cd5b2d24425d494858303619ae
fariaiago <111879008+fariaiago@users.noreply.github.com>
2023-05-18 22:26:02 +0000

    a

--- beecrowd/src/main.rs
+++ beecrowd/src/main.rs
@@ -1,25 +1,17 @@
 use std::io;
 
 fn main() {
-	let mut h1 = String::new();
-	let mut m1 = String::new();
-	let mut h2 = String::new();
-	let mut m2 = String::new();
+	let mut line = String::new();
 
-	io::stdin().read_line(&mut h1).unwrap();
-	io::stdin().read_line(&mut m1).unwrap();
-	io::stdin().read_line(&mut h2).unwrap();
-	io::stdin().read_line(&mut m2).unwrap();
+	io::stdin().read_line(&mut line).unwrap();
 
-	let h1 = h1.trim().parse::<i32>().unwrap();
-	let m1 = m1.trim().parse::<i32>().unwrap();
-	let h2 = h2.trim().parse::<i32>().unwrap();
-	let m2 = m2.trim().parse::<i32>().unwrap();
+	let inputs: Vec<i32> = line.split_whitespace().map(|x| x.parse().unwrap()).collect();
 
-	let t1 = h1 * 60 + m1;
-	let t2 = h2 * 60 + m2;
+	let t1 = inputs[0] * 60 + inputs[1];
+	let t2 = inputs[2] * 60 + inputs[3];
+
+	let h = if t1 == t2 { 24 } else { if t1 < t2 { (t2 - t1) / 60 } else { (1440 + t2 - t1) / 60 } };
+	let m = if t1 == t2 { 0 } else { if t1 < t2 { (t2 - t1) % 60 } else { (1440 + t2 - t1) % 60 } };
 	
-	println!("O JOGO DUROU {} HORA(S) E {} MINUTO(S)",
-		if t1 == t2 { 24 } else { if t1 < t2 { (t2 - t1) / 60 } else { (1440 + t2 - t1) / 60 } },
-		if t1 == t2 { 0 } else { if t1 < t2 { (t2 - t1) % 60 } else { (1440 + t2 - t1) % 60 } });
+	println!("O JOGO DUROU {} HORA(S) E {} MINUTO(S)", h, m);
 }
