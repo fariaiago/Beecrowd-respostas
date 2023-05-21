ba56ec5fc42d753e294a892ecd7a841d673b3fc9
fariaiago <111879008+fariaiago@users.noreply.github.com>
2023-05-21 10:56:23 +0000

    a

--- beecrowd/src/main.rs
+++ beecrowd/src/main.rs
@@ -1,17 +1,21 @@
 use std::io;
 
 fn main() {
-	let mut line = String::new();
-
-	io::stdin().read_line(&mut line).unwrap();
-
-	let inputs: Vec<i32> = line.split_whitespace().map(|x| x.parse().unwrap()).collect();
-
-	let t1 = inputs[0] * 60 + inputs[1];
-	let t2 = inputs[2] * 60 + inputs[3];
-
-	let h = if t1 == t2 { 24 } else { if t1 < t2 { (t2 - t1) / 60 } else { (1440 + t2 - t1) / 60 } };
-	let m = if t1 == t2 { 0 } else { if t1 < t2 { (t2 - t1) % 60 } else { (1440 + t2 - t1) % 60 } };
-	
-	println!("O JOGO DUROU {} HORA(S) E {} MINUTO(S)", h, m);
+	loop {
+		let mut line = String::new();
+		match io::stdin().read_line(&mut line) {
+			Ok(_) => {
+				let mut inputs: Vec<i32> = line.split_whitespace().map(|x| x.parse().unwrap()).collect();
+				inputs.sort();
+				if inputs.clone().into_iter().nth(0).unwrap() <= 0 { break; }
+				let mut sum = 0;
+				for n in inputs.clone().into_iter().nth(0).unwrap()..=inputs.clone().into_iter().nth(1).unwrap() {
+					sum += n;
+					print!("{} ", n);
+				}
+				println!("Sum={}", sum);
+			},
+			Err(_) => break
+		};
+	}
 }
